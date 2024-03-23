c4cf673910afa1086bed584b718f421738c678ef
Can1425 <101759631+Can1425@users.noreply.github.com>
2024-03-23 21:13:38 +0800

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,11 +3,3 @@ import DefaultTheme from 'vitepress/theme'
 import vpSearch from './components/vp-search.vue'
 import './style/index.scss'
 import type { VNode } from 'vue'
-
-/**
- * 使用第三方组件库
- * 
- * @see fighting-design https://github.com/FightingDesign/fighting-design
- */
-import FightingDesign from 'fighting-design'
-import 'fighting-design/dist/index.css'
