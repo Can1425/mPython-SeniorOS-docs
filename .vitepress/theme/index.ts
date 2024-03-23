1038b3ad635a95a068ed6398be74cb4795669dd0
Can1425 <101759631+Can1425@users.noreply.github.com>
2024-03-23 21:11:22 +0800

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -11,22 +11,3 @@ import type { VNode } from 'vue'
  */
 import FightingDesign from 'fighting-design'
 import 'fighting-design/dist/index.css'
-
-export default {
-  ...DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      /**
-       * 导航栏插入搜索的输入框插槽
-       *
-       * 更多插槽参考
-       *
-       * @see 布局插槽 https://vitepress.dev/zh/guide/extending-default-theme#layout-slots
-       */
-      'nav-bar-content-before': (): VNode => h(vpSearch)
-    })
-  },
-  enhanceApp({ app }) {
-    app.use(FightingDesign)
-  }
-}
